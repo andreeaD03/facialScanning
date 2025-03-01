b301d9760d0f9078f7ba142d3769d92b158b745d
unknown <andreeadan0302@gmail.com>
2025-03-01 18:25:03 +0200

    first attempt to build the Facial Scanning app

---
+++ detectingFaces.py
@@ -0,0 +1,23 @@
+import cv2
+# imagine = cv2.imread("image.jpeg")
+
+# edges = cv2.Canny(imagine, 50, 20)
+
+# cv2.imshow("Imaginea mea", edges)
+
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+imagine = cv2.imread("image22.jpg")
+grayImage = cv2.cvtColor(imagine, cv2.COLOR_BGR2GRAY)
+
+#detectam fetele
+faces = faceCascade.detectMultiScale(grayImage, scaleFactor=1.1, minNeighbors=7, minSize=(30, 30))
+
+ #desenam un dreptunghi in jurul fiecarei fete detectate
+for(x,y,w,h) in faces:
+    cv2.rectangle(imagine, (x,y), (x+w, y+h), (0,255,0), 2)
+
+
+cv2.imshow("detectare faciala", imagine)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
